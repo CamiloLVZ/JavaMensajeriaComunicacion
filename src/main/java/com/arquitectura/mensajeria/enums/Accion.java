0f2ee8077c253e19757bde2c33e978ae2427ca9f
Camilo <camilolon6@gmail.com>
2026-04-13 21:10:36 -0500

    primer commit

---
+++ src/main/java/com/arquitectura/mensajeria/enums/Accion.java
@@ -0,0 +1,10 @@
+package com.arquitectura.mensajeria.enums;
+
+public enum Accion {
+    CONECTAR,
+    LISTAR_CLIENTES,
+    LISTAR_DOCUMENTOS,
+    LISTAR_LOGS,
+    ENVIAR_DOCUMENTO,
+    OBTENER_DOCUMENTO
+}
